88142cc072d7d623a0f3e0a5f3e1eb2d8298702a
dilancitobaez <83933774+dilancitobaez@users.noreply.github.com>
2021-05-12 13:52:58 -0600

    Add files via upload

--- enviar2.php
+++ enviar2.php
@@ -1,12 +1,57 @@
-<?php 
-	$nombre = $_POST['nombre'];
-	$asunto = $_POST['asunto'];
-	$mensaje = $_POST['mensaje'];
+<?php
+$nombre = $_POST['nombre'];
+$mail = $_POST['mail'];
+$empresa = $_POST['empresa'];
 
-	echo $nombre. "ha dicho <br/>".$mensaje;
-	if(mail('edwinjosue56@hotmail.com', $asunto, $mensaje)){
-		echo "mail enviado";
-	}else{
-		echo "uyuyuyuyuy";
-	}
- ?>
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por... " . $nombre . ", de la empresa " . $empresa . " \r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Fue enviado... " . date('d/m/Y', time());
+
+$para = "edwinjosue56@hotmail.com";
+$asunto = 'contacto desde mi sitio';
+
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+
+?>
+</h2>
+<h2 align="center">Gracias!</h2>
+
+<p align="center">Tu mensaje ha sido enviado correctamente, pronto nos pondremos en contacto con usted.</p>
+<p align="center"> </p>
+<p><span style="color:red;font-size:150%;font-weight:bold;"><?php print $email; ?></span></p>
+
+<p align="center">Si no es correcto,
+
+<script type='text/javascript'>
+
+document.write('<a href="javascript:history.go(-1);">vuelve atras</a>');
+
+</script>
+
+<noscript>vuelve atras</noscript> y envialo de nuevo</p>
+
+<script type='text/javascript'>
+
+document.write('<p class="details"><a href="javascript:history.go(-2);">Volver a la página de inicio.</a></p>');
+
+</script>
+
+<script type='text/javascript'>
+
+setTimeout('history.go(-2)', 9000);
+
+</script>
+
+<noscript>
+
+<p align="center" class="details">Pulsa el boton "atras" en tu navegador para volver a la página anterior.</p>
+
+</noscript>
